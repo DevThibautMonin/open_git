a0f2ce725c0bb1beb593265f29be55125ca09fc3
Thibaut Monin <device.thibaut.monin@gmail.com>
2025-12-21 16:37:59 +0100

    chore(branches): remove unused bloc

--- lib/features/branches/presentation/ui/branch_item.dart
+++ lib/features/branches/presentation/ui/branch_item.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:open_git/features/branches/presentation/bloc/branches_bloc.dart';
-import 'package:open_git/shared/core/di/injectable.dart';
 import 'package:open_git/shared/domain/entities/branch_entity.dart';
 
 class BranchItem extends StatelessWidget {
@@ -16,28 +13,25 @@ class BranchItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<BranchesBloc>(),
-      child: MouseRegion(
-        cursor: SystemMouseCursors.click,
-        child: GestureDetector(
-          onTap: onTap,
-          child: Row(
-            children: [
-              Icon(
-                Icons.call_split,
-                size: 16,
-                color: branch.isCurrent ? Colors.blue : Colors.grey,
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Icon(
+              Icons.call_split,
+              size: 16,
+              color: branch.isCurrent ? Colors.blue : Colors.grey,
+            ),
+            const SizedBox(width: 8),
+            Expanded(
+              child: Text(
+                branch.name,
+                overflow: TextOverflow.ellipsis,
               ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Text(
-                  branch.name,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
