093c7dd3b86b18631279e9051d748471d500dd50
Thibaut Monin <device.thibaut.monin@gmail.com>
2026-01-05 18:47:19 +0100

    chore(ui): add more file type
    +gitignore
    +jenkins
    +php
    +blade.php

--- lib/shared/domain/enums/file_type_enum.dart
+++ lib/shared/domain/enums/file_type_enum.dart
@@ -8,6 +8,10 @@ enum FileTypeEnum {
   yaml(assetPath: "assets/file_type_icons/yaml.svg"),
   swift(assetPath: "assets/file_type_icons/swift.svg"),
   kotlin(assetPath: "assets/file_type_icons/kotlin.svg"),
+  php(assetPath: "assets/file_type_icons/php.svg"),
+  laravel(assetPath: "assets/file_type_icons/laravel.svg"),
+  git(assetPath: "assets/file_type_icons/git.svg"),
+  jenkins(assetPath: "assets/file_type_icons/jenkins.svg"),
 
   image(assetPath: "assets/file_type_icons/default_image.svg"),
   folder(assetPath: "assets/file_type_icons/default_file.svg"),
